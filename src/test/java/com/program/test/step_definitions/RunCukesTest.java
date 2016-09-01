d71685b6e4635c87488602cf823300fa4f97946e
Benjamin Harrington <ben.harrington@manheim.com>
2016-09-01 14:46:30 -0500

    quick initial test java selenium cuke

---
+++ src/test/java/com/program/test/step_definitions/RunCukesTest.java
@@ -0,0 +1,17 @@
+package step_definitions;
+
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  features = "classpath:features",
+  plugin = {"pretty", "html:target/cucumber-html-report"},
+  tags = {}
+)
+
+public class RunCukesTest {
+}
+
---
+++ src/test/resources/features/Google.feature
@@ -0,0 +1,6 @@
+Feature: Google Home Page
+
+  Scenario: Google Title Description
+    Given I visit Google
+    Then I see title Google
+
